6c674a55e32d9259f5c153ab42d4e0d007e7ea01
hwatton <66825106+hwatton@users.noreply.github.com>
2021-02-19 22:45:35 +0000

    trying to get interactive chart to work

--- src/components/interactiveGraph.jsx
+++ src/components/interactiveGraph.jsx
@@ -3,18 +3,33 @@ import { useState } from "react";
 //work out how to pass the value change back up to the state
 
 function InterGraph(props) {
-  const [data, setData] = useState([5, 7, 5]);
+  const startingData = [];
 
-  function newBallsPlease() {
-    let dt = [];
+  for (let i = 0; i < 10; i++) {
+    startingData.push({ cv: Math.floor(Math.random() * 10), id: "id_" + i });
+  }
+
+  const [data, setData] = useState(startingData);
+
+  function newBallsPlease({ target }) {
+    console.log(event);
+    let dt = data;
     for (let i = 0; i < 10; i++) {
-      dt.push(Math.floor(Math.random() * 10));
+      if (dt.id == target.id) {
+        dt[i].cv = event.data;
+      }
     }
     setData(dt);
   }
 
-  const peez = data.map((item) => (
-    <input onChange={newBallsPlease} type="text" value={item}></input>
+  const peez = data.map((item, i) => (
+    <input
+      id={item.id}
+      key={item + "_" + i}
+      onChange={newBallsPlease}
+      type="text"
+      value={item.cv}
+    ></input>
   ));
 
   return <div>{peez}</div>;
