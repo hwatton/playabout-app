20816e94709e402a990da474eb52898c65d26496
hwatton <66825106+hwatton@users.noreply.github.com>
2021-02-20 08:23:23 +0000

    got inputs working

--- src/components/interactiveGraph.jsx
+++ src/components/interactiveGraph.jsx
@@ -6,33 +6,38 @@ function InterGraph(props) {
   const startingData = [];
 
   for (let i = 0; i < 10; i++) {
-    startingData.push({ cv: Math.floor(Math.random() * 10), id: "id_" + i });
+    startingData.push({
+      cv: Math.floor(Math.random() * 10),
+      id: "id_" + i
+    });
   }
 
   const [data, setData] = useState(startingData);
 
-  function newBallsPlease({ target }) {
-    console.log(event);
-    let dt = data;
-    for (let i = 0; i < 10; i++) {
-      if (dt.id == target.id) {
-        dt[i].cv = event.data;
-      }
-    }
+  function newBallsPlease(e) {
+    //console.log(event.target.id);
+
+    let obj = data.find((x) => x.id === event.target.id);
+    let index = data.indexOf(obj);
+    let dt = [...data];
+    dt[index].cv = event.target.value;
+
     setData(dt);
+    console.log(data);
   }
 
   const peez = data.map((item, i) => (
     <input
+      className="graphInput"
       id={item.id}
       key={item + "_" + i}
-      onChange={newBallsPlease}
+      onChange={(e) => newBallsPlease(e)}
       type="text"
       value={item.cv}
     ></input>
   ));
 
-  return <div>{peez}</div>;
+  return <div className="inputHolder">{peez}</div>;
 }
 
 export default InterGraph;
