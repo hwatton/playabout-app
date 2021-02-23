30a861b8052135d1fa63be044e3fcbdd52cbc441
hwatton <66825106+hwatton@users.noreply.github.com>
2021-02-23 14:00:53 +0000

    Removing Consolelogs

--- src/components/motionDiv.jsx
+++ src/components/motionDiv.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 const Rotate = () => {
   const [isActive, setIsActive] = useState(false);
   const [isFinished, setIsFinished] = useState(false);
-  const constraintsRef = useRef()
+  const constraintsRef = useRef();
 
   const variant = {
     active: {
@@ -22,67 +22,69 @@ const Rotate = () => {
     }
   };
 
-  const [dragColour, setDragColour] = useState("white")
+  const [dragColour, setDragColour] = useState("white");
 
   return (
     <div className="md-holder">
-      <br/>
-      <button 
-      className="Button motion-button"
-      onClick={() => setIsActive(!isActive)}>{isActive ? "Drag it!" : "Find it!"}</button>
+      <br />
+      <button
+        className="Button motion-button"
+        onClick={() => setIsActive(!isActive)}
+      >
+        {isActive ? "Drag it!" : "Find it!"}
+      </button>
       <motion.div
         className="motionDiv"
         variants={variant}
-        
         animate={isActive ? "active" : "inactive"}
       >
-        
-        <div className="constrainingDiv"
-      ref={constraintsRef}
-      style={{
-        margin: "10%",
-        height: "80%",
-        width: "80%",
-      }}>
-        
-        <motion.div
-      style={{
-        left: 50,
-        width: 150,
-        height: 150,
-        borderRadius: 30,
-        backgroundColor: dragColour,
-        cursor: "grab",
-        alignItems: "center"
-      }}
-      drag
-      dragConstraints={constraintsRef} 
-      dragElastic={0.4}
-      onDrag = {(event, info)=>{
-        console.log(event)
-        console.log(info)
-        setIsFinished(false)
-        setDragColour("red")
-      }}
-      onDragEnd = {(event, info)=>{
-        console.log(event)
-        console.log(info)
-        setIsFinished(true)
-        setDragColour("white")
-        setTimeout(d=>{
-          setIsFinished(false)
-        }, 2000)
-
-
-      }}
-
-      whileTap={{ cursor: "grabbing" }}
-    ><div><p
-    style={{
-      color: "black"
-    }}>{isFinished ? "Thanks for that." : null}</p></div></motion.div></div>
+        <div
+          className="constrainingDiv"
+          ref={constraintsRef}
+          style={{
+            margin: "10%",
+            height: "80%",
+            width: "80%"
+          }}
+        >
+          <motion.div
+            style={{
+              left: 50,
+              width: 150,
+              height: 150,
+              borderRadius: 30,
+              backgroundColor: dragColour,
+              cursor: "grab",
+              alignItems: "center"
+            }}
+            drag
+            dragConstraints={constraintsRef}
+            dragElastic={0.4}
+            onDrag={() => {
+              setIsFinished(false);
+              setDragColour("red");
+            }}
+            onDragEnd={() => {
+              setIsFinished(true);
+              setDragColour("white");
+              setTimeout((d) => {
+                setIsFinished(false);
+              }, 2000);
+            }}
+            whileTap={{ cursor: "grabbing" }}
+          >
+            <div>
+              <p
+                style={{
+                  color: "black"
+                }}
+              >
+                {isFinished ? "Thanks for that." : null}
+              </p>
+            </div>
+          </motion.div>
+        </div>
       </motion.div>
-      
     </div>
   );
 };
