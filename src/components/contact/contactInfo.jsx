4883e348a97842cb934be3fef9f3832bfa36ca02
hwatton <harry.watton@hotmail.co.uk>
2021-03-09 22:07:52 +0000

    adding dragDiv

--- src/components/contact/contactInfo.jsx
+++ src/components/contact/contactInfo.jsx
@@ -1,6 +1,14 @@
+import {motion} from "framer-motion"
+import "./contact.css";
+import { useState }  from "react"
+
 function ContactInfo() {
+
+
+  const [showThing, setShowThing] = useState(false)
+
   return (
-    <div>
+    <div className="contactHolder">
       <p>Did it work this time?</p>
       <p>Did it work this time?</p>
       <p>Did it work this time?</p>
@@ -8,7 +16,17 @@ function ContactInfo() {
       <p>Did it work this time?</p>
       <p>Did it work this time?</p>
       <p>Did it work this time?</p>
+<div>
+      <motion.div
+      className="draggyThing"
+      drag="x"
+      dragConstraints={{ left: 0, right: 300 }}
+      onHoverStart={e => {setShowThing(true)}}
+      onHoverEnd={e => {setShowThing(false)}}
+      >
+        {showThing && <div><p>Thing!</p></div>}</motion.div></div>
     </div>
+    
   );
 }
 
