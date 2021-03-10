8c143b539618b4072d71e7e5b8dc1d78275a759a
hwatton <66825106+hwatton@users.noreply.github.com>
2021-03-10 13:57:35 +0000

    jstat

--- src/components/squares/squaredata.js
+++ src/components/squares/squaredata.js
@@ -1,30 +1,35 @@
-function sqData(){
+import * as jStat from "jstat";
 
-let beans = []
+function sqData() {
+  let beans = [];
 
-const tx = Math.floor(Math.random()*20)
-const ty = Math.floor(Math.random()*40)
+  const tx = Math.floor(Math.random() * 20);
+  const ty = Math.floor(Math.random() * 40);
 
-let colStep = 256/41
+  let colStep = 256 / 41;
+  let normalStuff = [];
 
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 40; j++) {
+      let dx = i - tx;
+      let dy = j - ty;
 
+      let hyp = hypo(dx, dy);
+      let tmp = jStat.normal.pdf(hyp, 20, 0.5);
 
-for (let i =0; i<20 ; i++) {
-    for (let j =0; j<40 ; j++) {
-
-        let dx = i - tx
-        let dy = j - ty
-
-    beans.push(hypo(dx,dy)*colStep)
-
+      beans.push(hypo(dx, dy) * colStep);
+      normalStuff.push({
+        bean: hyp * colStep,
+        norm: tmp
+      });
     }
+  }
+  console.log("this " + jStat.normal.pdf(5, 5, 2));
+  return normalStuff;
 }
 
-    return beans
+function hypo(x, y) {
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
-function hypo(x,y) {
-    return Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
-}
-
-export default sqData
+export default sqData;
--- src/components/squares/squares.jsx
+++ src/components/squares/squares.jsx
@@ -1,62 +1,58 @@
-import sqData from "./squaredata.js"
-import { useState } from "react"
-
+import sqData from "./squaredata.js";
+import { useState } from "react";
 
 function Squares() {
+  let grid = [];
+  const rNum = 20;
+  const num = rNum * rNum * 2;
 
-
-let grid = []
-const rNum = 20
-const num = rNum*rNum *2
-
-/*
+  /*
 for now: 
 grid is hardcoded 20 rows by 40 columns.
 */
 
-const [data, setData] = useState(sqData())
+  const [data, setData] = useState(sqData());
 
-function newData() {
-    let nD = sqData()
-    setData(nD)
-}
+  function newData() {
+    let nD = sqData();
+    setData(nD);
+  }
 
+  for (let i = 0; i < num; i++) {
+    grid.push(
+      <Square data={data[i].norm} clickFunc={newData} key={"key_" + i} />
+    );
+  }
 
-for (let i = 0;i<num;i++){
-
-    
-
-grid.push(<Square
-data={data[i]}
-clickFunc={newData}
-key={"key_" + i}/> )
-}
-
-const divWidth = 2 * ((rNum * 24) + 2)
-    return <div 
-    style={{
-        "display" : "flex",
-        "flexWrap" : "wrap",
-        "width" : divWidth,
-        "margin" : "10px"
-    }}>
-        {grid}
+  const divWidth = 2 * (rNum * 24 + 2);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        "min-width": divWidth,
+        "max-width": divWidth,
+        margin: "10px"
+      }}
+    >
+      {grid}
     </div>
+  );
 }
 
 function Square(props) {
-
-    return <div
-    onClick={props.clickFunc}
-    style={{
-        "width" : "20px",
-        "height" : "20px",
-        "margin" : "2px",
-        "backgroundColor": "rgb(" + (256 - props.data) + ", 0, " + (256 - props.data/2) + ")"
-}}></div>
+  return (
+    <div
+      onClick={props.clickFunc}
+      style={{
+        width: "20px",
+        height: "20px",
+        margin: "2px",
+        backgroundColor:
+          "rgb(" + (256 - props.data) + ", 0, " + (256 - props.data / 2) + ")"
+      }}
+    ></div>
+  );
 }
 
-
-
-
-export default Squares
+export default Squares;
